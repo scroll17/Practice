47ca36a7853a66ba7534c43c1f6be9bbd4e084c7
root <zololotarenko.2015@gmail.com>
2019-10-30 10:50:01 +0200

    add build

---
+++ build/precache-manifest.50ee1c432d887ab785cdd35aa281404e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9851a5ff4e4ac3e567c5d3e2ccdfb716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02dd49cdabe0b0184aa5",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "5dbab405467df96fc04d",
+    "url": "/static/css/main.6f091a6b.chunk.css"
+  },
+  {
+    "revision": "02dd49cdabe0b0184aa5",
+    "url": "/static/js/2.5c4da99a.chunk.js"
+  },
+  {
+    "revision": "5dbab405467df96fc04d",
+    "url": "/static/js/main.c6e013ae.chunk.js"
+  },
+  {
+    "revision": "805768ff9feb1f270266",
+    "url": "/static/js/runtime-main.16328109.js"
+  }
+]);
